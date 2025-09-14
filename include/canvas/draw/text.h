5fb785e75540e1a34f12394aee6b1fd88ba5a462
KorvusKroax <korvuskroax@gmail.com>
2025-09-14 18:17:57 +0200

    font: initial documentation added and some refracting

--- include/canvas/draw/text.h
+++ include/canvas/draw/text.h
@@ -1,12 +1,12 @@
 /*
-    draw_char(x, y, asciiCode, color, font)
+    draw_char(x, y, asciiCode, colors, font)
         - draw a charater of the given font to the passed coords, font are optional
             if no given font it will uses a simple 4x6 pixel charaterset
 
-    draw_line(x, y, text, color, font)
+    draw_line(x, y, text, colors, font)
         - write the given text using the given font to the passed coords, font are optional
             if no given font it will uses a simple 4x6 pixel charaterset
-            recognize and process the '\n' (newLine) character
+            recognize and apply the '\n' (newLine) character
 
     private calcTextField(text, &colCount, &rowCount)
         - give back row and max column count in charcount (not pixelcount)
--- include/canvas/font.h
+++ include/canvas/font.h
@@ -1,3 +1,18 @@
+/*
+    USAGE:
+        Font(fileName, charWidth)
+            - fileName: a .PNG file which contains the pixels of the charset
+                the first character must be the <space>
+            - charWidth: one monospaced character width in the .PNG file (optional)
+                if no passed charWidth the charset will be proportional
+
+    FUNCTIONS:
+        loadFont(*fileName, charWidth)
+            - loading charset from the .PNG file
+                the charset must be in one row ordered by ascii codes
+                - valid pixelcolor = 255, 255, 255, 255
+                - separator pixelcolor = 0, 0, 0, 255
+*/
 #pragma once
 
 #include "lodepng.h"
@@ -33,22 +48,15 @@ struct FontChar
 
 struct Font
 {
-    int charHeight, charCount = 0;
+    unsigned int charHeight, charCount = 0;
     FontChar** charset;
     FontType fontType;
 
     Font() { }
 
-    Font(const char *fileName) // proportional
-    {
-        loadFont(fileName);
-        fontType = PROPORTIONAL;
-    }
-
-    Font(const char *fileName, int charWidth) // monospaced
+    Font(const char *fileName, unsigned int charWidth = 0)
     {
         loadFont(fileName, charWidth);
-        fontType = MONOSPACED;
     }
 
     ~Font()
@@ -58,84 +66,94 @@ struct Font
         }
     }
 
-
-
-    bool loadFont(const char *fileName, int charWidth = -1)
+    bool loadFont(const char *fileName, unsigned int charWidth = 0)
     {
-        unsigned int fontSheetWidth, fontSheetHeight, channelCount = 4;
-        unsigned char *fontSheet;
-        unsigned error = lodepng_decode32_file(&fontSheet, &fontSheetWidth, &fontSheetHeight, fileName);
+        unsigned int fontSheetWidth, channelCount = 4;
+        unsigned char* fontSheet;
+        unsigned error = lodepng_decode32_file(&fontSheet, &fontSheetWidth, &this->charHeight, fileName);
         if (error) {
             std::cout << "loadFont(): decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
             return false;
         }
 
-        this->charHeight = fontSheetHeight;
+        if (charWidth > 0) {
+            return loadMonospaced(fontSheet, fontSheetWidth, channelCount, charWidth);
+        }
+        return loadProportional(fontSheet, fontSheetWidth, channelCount);
+    }
 
-        if (charWidth == -1) {
-            this->charCount = 0;
-            std::vector<int> charWidths;
-            int width = 0;
-            for (int i = 0; i < fontSheetWidth; i++) {
-                int r = fontSheet[i * channelCount + 0];
-                int g = fontSheet[i * channelCount + 1];
-                int b = fontSheet[i * channelCount + 2];
-                int a = fontSheet[i * channelCount + 3];
-                width++;
-                if (r == 0 && g == 0 && b == 0 && a != 0) {
-                    this->charCount++;
-                    charWidths.push_back(width - 1);
-                    width = 0;
+private:
+    bool loadMonospaced(unsigned char *fontSheet, unsigned int fontSheetWidth, unsigned int channelCount, int charWidth)
+    {
+        this->charCount = fontSheetWidth / charWidth;
+
+        if (this->charCount == 0) {
+            std::cout << "loadFont(): no monospaced character found" << std::endl;
+            return false;
+        }
+
+        this->charset = new FontChar*[this->charCount];
+
+        int xPos = 0;
+        for (int i = 0; i < this->charCount; i++) {
+            this->charset[i] = new FontChar(charWidth, this->charHeight);
+            for (int y = 0; y < this->charHeight; y++) {
+                for (int x = 0; x < charWidth; x++) {
+                    this->charset[i]->pixels[x + (this->charHeight - 1 - y) * charWidth] =
+                        fontSheet[((xPos + x) * channelCount + 0) + y * (fontSheetWidth * channelCount)] |
+                        fontSheet[((xPos + x) * channelCount + 1) + y * (fontSheetWidth * channelCount)] << 8 |
+                        fontSheet[((xPos + x) * channelCount + 2) + y * (fontSheetWidth * channelCount)] << 16 |
+                        fontSheet[((xPos + x) * channelCount + 3) + y * (fontSheetWidth * channelCount)] << 24;
                 }
             }
+            xPos += charWidth;
+        }
 
-            if (this->charCount == 0) {
-                std::cout << "loadFont(): no proportional character found" << std::endl;
-                return false;
-            }
+        this->fontType = MONOSPACED;
+        return true;
+    }
 
-            this->charset = new FontChar*[this->charCount];
-
-            int xPos = 0;
-            for (int i = 0; i < this->charCount; i++) {
-                this->charset[i] = new FontChar(charWidths[i], this->charHeight);
-                for (int y = 0; y < this->charHeight; y++) {
-                    for (int x = 0; x < charWidths[i]; x++) {
-                        this->charset[i]->pixels[x + (this->charHeight - 1 - y) * charWidths[i]] =
-                            fontSheet[((xPos + x) * channelCount + 0) + y * (fontSheetWidth * channelCount)] |
-                            fontSheet[((xPos + x) * channelCount + 1) + y * (fontSheetWidth * channelCount)] << 8 |
-                            fontSheet[((xPos + x) * channelCount + 2) + y * (fontSheetWidth * channelCount)] << 16 |
-                            fontSheet[((xPos + x) * channelCount + 3) + y * (fontSheetWidth * channelCount)] << 24;
-                    }
-                }
-                xPos += charWidths[i] + 1;
-            }
-        } else {
-            this->charCount = fontSheetWidth / charWidth;
-
-            if (this->charCount == 0) {
-                std::cout << "loadFont(): no monospaced character found" << std::endl;
-                return false;
-            }
-
-            this->charset = new FontChar*[this->charCount];
-
-            int xPos = 0;
-            for (int i = 0; i < this->charCount; i++) {
-                this->charset[i] = new FontChar(charWidth, this->charHeight);
-                for (int y = 0; y < this->charHeight; y++) {
-                    for (int x = 0; x < charWidth; x++) {
-                        this->charset[i]->pixels[x + (this->charHeight - 1 - y) * charWidth] =
-                            fontSheet[((xPos + x) * channelCount + 0) + y * (fontSheetWidth * channelCount)] |
-                            fontSheet[((xPos + x) * channelCount + 1) + y * (fontSheetWidth * channelCount)] << 8 |
-                            fontSheet[((xPos + x) * channelCount + 2) + y * (fontSheetWidth * channelCount)] << 16 |
-                            fontSheet[((xPos + x) * channelCount + 3) + y * (fontSheetWidth * channelCount)] << 24;
-                    }
-                }
-                xPos += charWidth;
+    bool loadProportional(unsigned char *fontSheet, unsigned int fontSheetWidth, int channelCount)
+    {
+        this->charCount = 0;
+        std::vector<int> charWidths;
+        int width = 0;
+        for (int i = 0; i < fontSheetWidth; i++) {
+            int r = fontSheet[i * channelCount + 0];
+            int g = fontSheet[i * channelCount + 1];
+            int b = fontSheet[i * channelCount + 2];
+            int a = fontSheet[i * channelCount + 3];
+            width++;
+            if (r == 0 && g == 0 && b == 0 && a != 0) {
+                this->charCount++;
+                charWidths.push_back(width - 1);
+                width = 0;
             }
         }
 
+        if (this->charCount == 0) {
+            std::cout << "loadFont(): no proportional character found" << std::endl;
+            return false;
+        }
+
+        this->charset = new FontChar*[this->charCount];
+
+        int xPos = 0;
+        for (int i = 0; i < this->charCount; i++) {
+            this->charset[i] = new FontChar(charWidths[i], this->charHeight);
+            for (int y = 0; y < this->charHeight; y++) {
+                for (int x = 0; x < charWidths[i]; x++) {
+                    this->charset[i]->pixels[x + (this->charHeight - 1 - y) * charWidths[i]] =
+                        fontSheet[((xPos + x) * channelCount + 0) + y * (fontSheetWidth * channelCount)] |
+                        fontSheet[((xPos + x) * channelCount + 1) + y * (fontSheetWidth * channelCount)] << 8 |
+                        fontSheet[((xPos + x) * channelCount + 2) + y * (fontSheetWidth * channelCount)] << 16 |
+                        fontSheet[((xPos + x) * channelCount + 3) + y * (fontSheetWidth * channelCount)] << 24;
+                }
+            }
+            xPos += charWidths[i] + 1;
+        }
+
+        this->fontType = PROPORTIONAL;
         return true;
     }
 };
