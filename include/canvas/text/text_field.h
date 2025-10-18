2f7afbbf0214dd3802bfcd203fefeb7f53242fd8
KorvusKroax <korvuskroax@gmail.com>
2025-10-18 19:04:11 +0200

    text_field: refactored

--- include/canvas/text/text_field.h
+++ include/canvas/text/text_field.h
@@ -5,163 +5,139 @@
 #include "color.h"
 #include "text/text.h"
 
-#include "rectangle.h"
-
 #include <string>
 #include <vector>
 
 struct TextField
 {
     int width, height;
-    std::string text;
     Font *font;
-    Color color;
-    float lineSpacingMultiplier;
 
-    TextField(int width, int height, std::string text, Font *font, Color color, float lineSpacingMultiplier = 1.0f):
-        width(width), height(height), text(text), font(font), color(color), lineSpacingMultiplier(lineSpacingMultiplier)
+    TextField(int width, int height, Font *font, std::string text):
+        width(width), height(height), font(font)
     {
-        std::vector<std::string> rows = explodeByChar(this->text, '\n');
-        for (std::string row : rows) {
-            std::string currentLine;
+        std::vector<std::vector<int>> rows = explodeByCodepoint(this->font->utf8ToCodepoints(text), '\n');
+        for (std::vector<int> row : rows) {
+            std::vector<std::vector<int>> words = explodeByCodepoint(row, ' ');
+
+            std::vector<int> currentLine;
             float currentLineWidth = 0;
-            std::vector<std::string> words = explodeByChar(row, ' ');
-            for (std::string word : words) {
+
+            for (std::vector<int> word : words) {
                 float wordWidth = getTextWidth(word);
 
-                // check if the word longest than the field
+                if (wordWidth == 0) continue;
+
                 if (wordWidth > this->width) {
-                    if (!currentLine.empty()) {
-                        // finish the line if not empty and clear the next
+                    if (currentLineWidth > 0) {
                         lines.push_back(std::make_pair(currentLine, currentLineWidth));
                         currentLine.clear();
+                        currentLineWidth = 0;
                     }
 
-                    // split long word more lines
-                    std::vector<std::pair<std::string, float>> splittedWords = explodeByWidth(word);
-                    for (int i = 0; i < splittedWords.size() - 1; i++) {
-                        lines.push_back(std::make_pair(splittedWords[i].first, splittedWords[i].second));
+                    std::vector<std::pair<std::vector<int>, float>> splittedLongWordParts = explodeByFieldWidth(word);
+                    for (int i = 0; i < splittedLongWordParts.size() - 1; i++) {
+                        lines.push_back(std::make_pair(splittedLongWordParts[i].first, splittedLongWordParts[i].second));
                     }
-                    // long word's end added to current line
-                    currentLine = splittedWords[splittedWords.size() - 1].first;
-                    currentLineWidth = splittedWords[splittedWords.size() - 1].second;
+                    currentLine = splittedLongWordParts.back().first;
+                    currentLineWidth = splittedLongWordParts.back().second;
                     continue;
                 }
 
-                if (currentLine.empty()) {
-                    // first word in the line
+                if (currentLineWidth == 0) {
                     currentLine = word;
                     currentLineWidth = wordWidth;
                     continue;
                 }
 
-                int spaceWidth = getSpaceWidthWithKerning((unsigned char)currentLine.back(), (unsigned char)word.front());
+                float spaceWidth = (
+                    stbtt_GetCodepointKernAdvance(&this->font->info, currentLine.back(), ' ') +
+                    this->font->getGlyph(' ')->advanceWidth +
+                    stbtt_GetCodepointKernAdvance(&this->font->info, ' ', word.front())
+                ) * this->font->scale;
+
                 if (currentLineWidth + spaceWidth + wordWidth <= this->width) {
-                    // adding next word to the line
-                    currentLine += " " + word;
+                    currentLine.push_back(' ');
+                    currentLine.insert(currentLine.end(), word.begin(), word.end());
                     currentLineWidth += spaceWidth + wordWidth;
                     continue;
                 }
 
-                // finish line and start next line
                 lines.push_back(std::make_pair(currentLine, currentLineWidth));
                 currentLine = word;
                 currentLineWidth = wordWidth;
             }
 
-            // finish the last line
             lines.push_back(std::make_pair(currentLine, currentLineWidth));
         }
     }
 
-    void draw(Canvas *canvas, int x, float y)
+    void draw(Canvas *canvas, int x, float y, Color color, float lineSpacingMultiplier = 1.0f)
     {
-        Rectangle::draw(canvas, x, y, this->width, this->height, C64_VICE_RED);
-
-        float lineHeight = (this->font->ascent - this->font->descent + this->font->lineGap) * this->font->scale * this->lineSpacingMultiplier;
+        float lineHeight = (this->font->ascent - this->font->descent + this->font->lineGap) * this->font->scale * lineSpacingMultiplier;
 
         y += (this->lines.size() - 1) * lineHeight;
 
-        for (std::pair<std::string, float> line : this->lines) {
-            Text::draw_line(canvas, x, y, line.first, this->font, this->color, CLEAR);
+        for (std::pair<std::vector<int>, float> line : this->lines) {
+            Text::draw_line(canvas, x, y, line.first, this->font, color, CLEAR);
             y -= lineHeight;
         }
     }
 
 private:
-    std::vector<std::pair<std::string, float>> lines;
+    std::vector<std::pair<std::vector<int>, float>> lines;
 
-    std::vector<std::string> explodeByChar(std::string text, char separator)
+    std::vector<std::vector<int>> explodeByCodepoint(const std::vector<int> &text, int separator)
     {
-        std::vector<std::string> result;
+        std::vector<std::vector<int>> result;
 
-        size_t start = 0, end;
-        while ((end = text.find(separator, start)) != std::string::npos) {
-            result.push_back(text.substr(start, end - start));
-            start = end + 1;
+        std::vector<int> textPart;
+        for (int codepoint : text) {
+            if (codepoint == separator) {
+                result.push_back(textPart);
+                textPart.clear();
+            } else {
+                textPart.push_back(codepoint);
+            }
         }
-        result.push_back(text.substr(start));
+        result.push_back(textPart);
 
         return result;
     }
 
-    float getTextWidth(std::string text)
+    float getTextWidth(const std::vector<int> &text)
     {
-        std::vector<int> codepoints = this->font->utf8ToCodepoints(text);
+        float textWidth = 0;
 
-        Glyph *nextGlyph, *currGlyph = this->font->getGlyph(codepoints[0]);
-        float textWidth = currGlyph->advanceWidth * this->font->scale;
-
-        for (int i = 1; i < codepoints.size(); i++) {
-            nextGlyph = this->font->getGlyph(codepoints[i]);
+        for (int i = 0; i < text.size(); i++) {
             textWidth +=
-                stbtt_GetGlyphKernAdvance(&this->font->info, currGlyph->index, nextGlyph->index) * this->font->scale +
-                nextGlyph->advanceWidth * this->font->scale;
-            currGlyph = nextGlyph;
+                (i ? stbtt_GetCodepointKernAdvance(&this->font->info, text[i - 1], text[i]) : 0) +
+                this->font->getGlyph(text[i])->advanceWidth;
         }
 
-        return textWidth;
+        return textWidth * this->font->scale;
     }
 
-    float getSpaceWidthWithKerning(unsigned char lastChar, unsigned char firstChar)
+    std::vector<std::pair<std::vector<int>, float>> explodeByFieldWidth(const std::vector<int> &text)
     {
-        Glyph *lastGlyph = this->font->getGlyph(lastChar); // this->font->utf8ToCodepoint(lastChar)
-        Glyph *spaceGlyph = this->font->getGlyph(' '); // this->font->utf8ToCodepoint(" ")
-        Glyph *firstGlyph = this->font->getGlyph(firstChar); // this->font->utf8ToCodepoint(firstChar)
+        std::vector<std::pair<std::vector<int>, float>> result;
 
-        float kerningLeft  = stbtt_GetGlyphKernAdvance(&this->font->info, lastGlyph->index, spaceGlyph->index) * this->font->scale;
-        int spaceWidth = spaceGlyph->advanceWidth * this->font->scale;
-        float kerningRight = stbtt_GetGlyphKernAdvance(&this->font->info, spaceGlyph->index, firstGlyph->index) * this->font->scale;
+        std::vector<int> line;
+        float lineWidth = 0;
+        for (int i = 0; i < text.size(); i++) {
 
-        return kerningLeft + spaceWidth + kerningRight;
-    }
+            int kern = i ? stbtt_GetCodepointKernAdvance(&this->font->info, text[i - 1], text[i]) * this->font->scale : 0;
+            float charWidth = this->font->getGlyph(text[i])->advanceWidth * this->font->scale;
 
-    std::vector<std::pair<std::string, float>> explodeByWidth(std::string text)
-    {
-        std::vector<std::pair<std::string, float>> result;
-
-        std::vector<int> codepoints = this->font->utf8ToCodepoints(text);
-
-        Glyph *nextGlyph, *currGlyph = this->font->getGlyph(codepoints[0]);
-        std::string line(1, text[0]);
-        float lineWidth = currGlyph->advanceWidth * this->font->scale;
-
-        for (int i = 1; i < text.size(); i++) {
-            nextGlyph = this->font->getGlyph(codepoints[i]);
-            float charWidth =
-                stbtt_GetGlyphKernAdvance(&this->font->info, currGlyph->index, nextGlyph->index) * this->font->scale +
-                nextGlyph->advanceWidth * this->font->scale;
-
-            if (lineWidth + charWidth > this->width) {
+            if (lineWidth + kern + charWidth > this->width) {
                 result.push_back(std::make_pair(line, lineWidth));
-                line = text[i];
+                line.assign(1, text[i]);
                 lineWidth = charWidth;
                 continue;
             }
 
-            line += text[i];
-            lineWidth += charWidth;
-            currGlyph = nextGlyph;
+            line.push_back(text[i]);
+            lineWidth += kern + charWidth;
         }
         result.push_back(std::make_pair(line, lineWidth));
 
